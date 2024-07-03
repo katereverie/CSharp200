f2289003dd2f2118ac979f4c37729dae27702434
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-07-03 16:29:24 +0200

    upload

---
+++ Experiments/Experiments/TypesClassesObjects/Program.cs
@@ -0,0 +1,12 @@
+﻿using TypesClassesObjects;
+
+TemplateClass testerObj = new TemplateClass();
+
+if (testerObj._iAmPublic)
+{
+    Console.WriteLine("A public field has been accessed.");
+}
+
+// a public static field can only be accessed via the Class, not its objects.
+var staticValue = TemplateClass._iAmPublicStatic;
+Console.WriteLine(staticValue);
---
+++ Experiments/Experiments/TypesClassesObjects/TemplateClass.cs
@@ -0,0 +1,16 @@
+﻿namespace TypesClassesObjects
+{
+    public class TemplateClass
+    {
+        public bool _iAmPublic;
+        private bool _iAmPrivate;
+        public static int _iAmPublicStatic;
+
+        public TemplateClass()
+        {
+            _iAmPublic = true;
+            _iAmPrivate = true;
+            _iAmPublicStatic = 99;
+        }
+    }
+}
