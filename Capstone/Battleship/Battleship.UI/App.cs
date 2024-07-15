1a8f6c1e4a3a8b60b7dc6c0346b4193350dd3f4b
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-07-15 17:46:58 +0200

    Add constructor, simplify workflow

--- Capstone/Battleship/Battleship.UI/App.cs
+++ Capstone/Battleship/Battleship.UI/App.cs
@@ -12,7 +12,13 @@ namespace Battleship.UI
         private IPlayer _p1;
         private IPlayer _p2;
 
-        private void SetUpShip(IPlayer player, Ship ship)
+        public App ()
+        {
+            _p1 = PlayerFactory.GetPlayer(1);
+            _p2 = PlayerFactory.GetPlayer(2);
+        }
+
+        private void PlaceShip(IPlayer player, Ship shipToPlace)
         {
             if (player.IsHuman)
             {
@@ -26,27 +32,28 @@ namespace Battleship.UI
                 // exit loop only if player's placement is valid under all conditions
                 while (true)
                 {
-                    Console.WriteLine($"Ship to place: {ship.Name} | Size: {ship.Size}");
-                    Coordinate startingCoordinate = player.DecideCoordinate("Enter a starting coordinate (e.g. A1): ");
-                    char direction = player.DecideDirection();
-                    ship.SetCoordinates(startingCoordinate, direction);
+                    Console.WriteLine($"Ship to place: {shipToPlace.Name} | Size: {shipToPlace.Size}");
+                    Coordinate startingCoordinate = player.GetCoordinate("Enter a starting coordinate (e.g. A1): ");
+                    char direction = player.GetDirection();
 
-                    if (_mgr.CheckOffgridShip(ship) == PlacementResult.Offgrid)
+                    if (_mgr.CheckOffgridShip(startingCoordinate, shipToPlace.Size, direction) == PlacementResult.Offgrid)
                     {
                         GameConsole.PrintErrorMessage("Ship Offgrid.\n");
                         continue;
                     }
 
-                    if (_mgr.CheckOverlapShip(ship, player.Ships) == PlacementResult.Overlap)
+                    shipToPlace.SetCoordinates(startingCoordinate, direction);
+
+                    if (_mgr.CheckOverlapShip(shipToPlace, player.Ships) == PlacementResult.Overlap)
                     {
                         GameConsole.PrintErrorMessage("Ship Overlap.\n");
                         continue;
                     }
 
                     // since placed ship is neither offgrid nor overlapped, add it to player's ships and print the game board
-                    player.AddShip(ship);
-                    Console.WriteLine($"You have successfully placed your {ship.Name}.");
-                    if (ship.Symbol == 'D')
+                    player.PlaceShip(shipToPlace);
+                    Console.WriteLine($"You have successfully placed your {shipToPlace.Name}.");
+                    if (shipToPlace.Symbol == 'D')
                     {
                         shipBoard = _mgr.MapShipsToBoard(shipBoard, player.Ships);
                         GameConsole.PrintBoard(shipBoard);
@@ -61,77 +68,45 @@ namespace Battleship.UI
             {
                 while (true)
                 {
-                    char direction = player.DecideDirection();
-                    Coordinate startingCoordinate = player.DecideCoordinate();
-                    ship.SetCoordinates(startingCoordinate, direction);
-
-                    if (_mgr.CheckOffgridShip(ship) == PlacementResult.Offgrid)
+                    char dir = player.GetDirection();
+                    Coordinate startingCoord = player.GetCoordinate("");
+                    if (_mgr.CheckOffgridShip(startingCoord, shipToPlace.Size, dir) == PlacementResult.Offgrid)
                     {
                         continue;
                     }
 
-                    if (_mgr.CheckOverlapShip(ship, player.Ships) == PlacementResult.Overlap)
+                    shipToPlace.SetCoordinates(startingCoord, dir);
+
+                    if (_mgr.CheckOverlapShip(shipToPlace, player.Ships) == PlacementResult.Overlap)
                     {
                         continue;
                     }
 
-                    player.AddShip(ship);
+                    player.PlaceShip(shipToPlace);
                     return;
                 }
             }
         }
 
-        private void SetUpPlayer(IPlayer player)
+        private void SetUpPlayer(IPlayer p)
         {
-            SetUpShip(player, new AircraftCarrier());
-            SetUpShip(player, new BattleShip());
-            SetUpShip(player, new Cruiser());
-            SetUpShip(player, new Submarine());
-            SetUpShip(player, new Destroyer());
-        }
-
-        private void SelectPlayer()
-        {
-            while (true)
-            {
-                Console.WriteLine("Welcome to Battleship where only the best Captain survives!\n");
-                GameConsole.PrintPlayerSelectionRules();
-
-                _p1 = PlayerFactory.GetPlayer("First player - (H)uman or (C)omputer? Your choice: ");
-                _p2 = PlayerFactory.GetPlayer("Second player - (H)uman or (C)omputer? Your choice: ");
-
-                if (!_p1.IsHuman && !_p2.IsHuman)
-                {
-                    GameConsole.PrintErrorMessage("At least one player must be human.");
-                    Console.WriteLine("Press any key to continue...");
-                    Console.ReadKey();
-                    Console.Clear();
-                    continue;
-                }
-
-                break;
-            }
+            PlaceShip(p, new AircraftCarrier());
+            PlaceShip(p, new BattleShip());
+            PlaceShip(p, new Cruiser());
+            PlaceShip(p, new Submarine());
+            PlaceShip(p, new Destroyer());
         }
 
         private IPlayer GetNextPlayer(IPlayer currentPlayer)
         {
-            if (currentPlayer == _p1)
-            {
-                return _p2;
-            }
-
-            return _p1;
+            return currentPlayer == _p1? _p2 : _p1;
         }
 
         public void Run()
         {
-            // set up game
-            SelectPlayer();
-            Console.Clear();
-
+            GameConsole.AnyKey();
             SetUpPlayer(_p1);
             SetUpPlayer(_p2);
-            Console.Clear();
 
             IPlayer currentPlayer = _startDecider.Next(1, 3) == 1 ? _p1 : _p2;
             IPlayer nextPlayer;
@@ -151,22 +126,17 @@ namespace Battleship.UI
                 Console.ResetColor();
                 Console.WriteLine("'s turn.\n");
 
-                if (currentPlayer.IsHuman)
-                {
-                    GameConsole.PrintBoard(currentPlayer.GameBoard);
-                }
+                GameConsole.PrintBoard(currentPlayer.ShotBoard);
 
                 // Placing shot
-                PlacementResult placementResult;
                 ShotResult shotResult;
-                Coordinate validShot;
+                Coordinate shot;
 
                 while (true)
                 {
-                    Coordinate targetShot = currentPlayer.IsHuman ? currentPlayer.DecideCoordinate("Enter target coordinate (e.g. A5): ") : currentPlayer.DecideCoordinate();
-                    placementResult = _mgr.CheckOverlapShot(targetShot, currentPlayer.Shots);
+                    shot = currentPlayer.IsHuman ? currentPlayer.GetCoordinate("Enter target coordinate (e.g. A5): ") : currentPlayer.GetCoordinate("");
 
-                    if (placementResult == PlacementResult.Overlap)
+                    if (_mgr.CheckOverlapShot(shot, currentPlayer.Shots) == PlacementResult.Overlap)
                     {
                         if (currentPlayer.IsHuman)
                         {
@@ -176,16 +146,14 @@ namespace Battleship.UI
                         continue;
                     }
 
-                    validShot = targetShot;
-                    currentPlayer.PlaceShot(targetShot);
+                    currentPlayer.Shots.Add(shot);
                     break;
                 }
 
+                Console.WriteLine($"{currentPlayer.Name} fires a shot at {shot}...");
+                shotResult = _mgr.EvaluateValidShot(shot, nextPlayer.Ships);
 
-                Console.WriteLine($"{currentPlayer.Name} fires a shot at {validShot}...");
-                shotResult = _mgr.EvaluateValidShot(validShot, nextPlayer.Ships);
-
-                int index = Coordinate.ToBoardIndex(validShot);
+                int index = Coordinate.ToBoardIndex(shot);
 
                 switch (shotResult)
                 {
@@ -205,27 +173,21 @@ namespace Battleship.UI
 
                 Console.ResetColor();
 
-                if (currentPlayer.IsHuman)
-                {
-                    currentPlayer.GameBoard[index] = shotResult == ShotResult.Miss ? 'M' : 'H';
-                }
+                currentPlayer.ShotBoard[index] = shotResult == ShotResult.Miss ? 'M' : 'H';
 
                 // exit game statement
                 if (_mgr.CalculateRemainingShips(nextPlayer.Ships) == 0)
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine($"{currentPlayer.Name}, You are the Victor!");
+                    Console.WriteLine($"{currentPlayer.Name} is the Victor!");
                     Console.ResetColor();
                     return;
                 }
 
-                Console.Write("Press any key to continue...");
-                Console.ReadKey();
-                Console.Clear();
+                GameConsole.AnyKey();
                 GameConsole.PrintGameRules();
                 currentPlayer = GetNextPlayer(currentPlayer);
             }
-
         }
     }
 }
