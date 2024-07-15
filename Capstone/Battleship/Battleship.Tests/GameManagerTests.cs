c8eb252967be037720836cf78d890a217700fb81
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-07-15 17:48:23 +0200

    update tests

--- Capstone/Battleship/Battleship.Tests/GameManagerTests.cs
+++ Capstone/Battleship/Battleship.Tests/GameManagerTests.cs
@@ -1,7 +1,7 @@
 ﻿using NUnit.Framework;
+using Battleship.BLL;
 using Battleship.BLL.Ships;
 using Battleship.Tests.TestModels.DefaultShips;
-using Battleship.BLL;
 using Battleship.Tests.TestModels;
 
 namespace Battleship.Tests
@@ -14,18 +14,19 @@ namespace Battleship.Tests
         [Test]
         public void TestShipPlacement_Offgrid()
         {
-            var ship1 = new BattleShip();
-            var ship2 = new DefaultBattleShip();
+            var startCoord1 = new Coordinate(1, 8); // A8
+            var startCoord2 = new Coordinate(6, 8); // F8
+            var startCoord3 = new Coordinate(5, 5); // E5
 
-            var startCoord = new Coordinate(1, 8); // A8
-
-            ship1.SetCoordinates(startCoord, 'V');
-
-            var result1 = _mgr.CheckOffgridShip(ship1);
-            var result2 = _mgr.CheckOffgridShip(ship2);
+            var result1 = _mgr.CheckOffgridShip(startCoord1, 5, 'V');
+            var result2 = _mgr.CheckOffgridShip(startCoord2, 6, 'H');
+            var result3 = _mgr.CheckOffgridShip(startCoord3, 6, 'V');
+            var result4 = _mgr.CheckOffgridShip(startCoord3, 6, 'H');
 
             Assert.That(result1, Is.EqualTo(PlacementResult.Offgrid));
-            Assert.That(result2, Is.EqualTo(PlacementResult.Placed));
+            Assert.That(result2, Is.EqualTo(PlacementResult.Offgrid));
+            Assert.That(result3, Is.EqualTo(PlacementResult.Placed));
+            Assert.That(result4, Is.EqualTo(PlacementResult.Placed));
         }
 
         [Test]
@@ -36,7 +37,7 @@ namespace Battleship.Tests
 
             var startCoord = new Coordinate(1, 6); // A6
 
-            var repo = new ShipRepository();
+            var repo = new DefaultShipRepository();
             ship1.SetCoordinates(startCoord, 'H');
 
             var result1 = _mgr.CheckOverlapShip(ship1, repo.Ships);
@@ -53,7 +54,7 @@ namespace Battleship.Tests
             var shot1 = new Coordinate(1, 1); // A1
             var shot2 = new Coordinate(1, 2); // A2
 
-            var repo = new ShotRepository(); 
+            var repo = new DefaultShotHistory(); 
 
             var result1 = _mgr.CheckOverlapShot(shot1, repo.Shots);
             var result2 = _mgr.CheckOverlapShot(shot2, repo.Shots);
@@ -70,7 +71,7 @@ namespace Battleship.Tests
             var shot2 = new Coordinate(2, 5); // B5
             var shot3 = new Coordinate(3, 4); // C4
 
-            var repo = new ShipRepository();
+            var repo = new DefaultShipRepository();
 
             var result1 = _mgr.EvaluateValidShot(shot1, repo.Ships);
             var result2 = _mgr.EvaluateValidShot(shot2, repo.Ships);
@@ -88,7 +89,7 @@ namespace Battleship.Tests
             var shot2 = new Coordinate(2, 1); // B1
             var shot3 = new Coordinate(3, 1); // C1
 
-            var repo = new ShipRepository();
+            var repo = new DefaultShipRepository();
 
             var result1 = _mgr.EvaluateValidShot(shot1, repo.Ships);
             var result2 = _mgr.EvaluateValidShot(shot2, repo.Ships);
@@ -104,7 +105,7 @@ namespace Battleship.Tests
             var shot1 = new Coordinate(2, 1); //B1
             var shot2 = new Coordinate(1, 1); //A1
 
-            var repo = new ShipRepository();
+            var repo = new DefaultShipRepository();
 
             repo.Ships[0].CountHit();
             repo.Ships[0].CountHit();
@@ -121,7 +122,7 @@ namespace Battleship.Tests
         [Test]
         public void TestCalculation_RemainingShips()
         {
-            var repo = new ShipRepository();
+            var repo = new DefaultShipRepository();
 
             var shot1 = new Coordinate(1, 1); // A1
             var shot2 = new Coordinate(1, 2); // A2
@@ -147,7 +148,7 @@ namespace Battleship.Tests
         [Test] 
         public void TestCalculation_RemainingHits()
         {
-            var repo = new ShipRepository(); // 17
+            var repo = new DefaultShipRepository(); // 17
 
             var calResult1 = _mgr.CalculateRemainingHits(repo.Ships);
 
