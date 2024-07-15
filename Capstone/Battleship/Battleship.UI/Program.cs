b129a5151430f75c2d48778115803a7dd8e017a5
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-07-15 17:48:11 +0200

    move welcome message to program

--- Capstone/Battleship/Battleship.UI/Program.cs
+++ Capstone/Battleship/Battleship.UI/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Battleship.UI;
 
+Console.WriteLine("Welcome to Battleship where only the best Captain survives!\n");
+GameConsole.PrintPlayerSelectionRules();
 App app = new App();
 app.Run();
